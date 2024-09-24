984f8aea3fbeea2e63eb11b50ab6d63e86b6b581
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-24 20:17:56 +0300

    Register models in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Recipient, FileSendingProcess, File
 
-# Register your models here.
+admin.site.register(Recipient)
+admin.site.register(FileSendingProcess)
+admin.site.register(File)
