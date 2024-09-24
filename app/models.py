2ba6b55041e5f84083a642af2831ad0e33ad81fc
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-24 14:53:33 +0300

    Update models

--- app/models.py
+++ app/models.py
@@ -10,12 +10,21 @@ class Recipient(models.Model):
         ('D', 'Deleted')
     ]
     name = models.CharField(max_length=90)
-    desc = models.CharField(max_length=140)
+    desc = models.CharField(max_length=140, blank=True)
     phone = models.CharField(max_length=18)
     city = models.CharField(max_length=40)
     birthdate = models.DateField(blank=True, null=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     avatar = models.URLField(blank=True, null=True)
+    uni = models.CharField(max_length=140, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FileSendingProcessManager(models.Manager):
+    def get_draft(self, user_id: int):
+        return self.get(status='DRF', sender=user_id)
 
 
 class FileSendingProcess(models.Model):
@@ -33,6 +42,8 @@ class FileSendingProcess(models.Model):
     sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='sender')
     moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='moderator')
     recipients = models.ManyToManyField(Recipient, through="FileSendingProcessRecipient")
+
+    objects = FileSendingProcessManager()
 
     class Meta:
         constraints = [
