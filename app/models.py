6061e426a091435444ddec20acff4c01d1cf4918
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-24 11:25:47 +0300

    Create models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,52 @@
+import django.db.models
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Recipient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('A', 'Active'),
+        ('D', 'Deleted')
+    ]
+    name = models.CharField(max_length=90)
+    desc = models.CharField(max_length=140)
+    phone = models.CharField(max_length=18)
+    city = models.CharField(max_length=40)
+    birthdate = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    avatar = models.URLField(blank=True, null=True)
+
+
+class FileSendingProcess(models.Model):
+    STATUS_CHOICES = [
+        ('DRF', 'Draft'),
+        ('DEL', 'Deleted'),
+        ('FRM', 'Formed'),
+        ('COM', 'Completed'),
+        ('REJ', 'Rejected')
+    ]
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    formed_at = models.DateTimeField(blank=True, null=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='sender')
+    moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='moderator')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['sender'], condition=models.Q(status='draft'),
+                                    name='unique_draft_per_sender')
+        ]
+
+
+class FileSendingProcessRecipient(models.Model):
+    file_sending_process = models.ForeignKey(FileSendingProcess, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(Recipient, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['file_sending_process', 'recipient', 'comment'],
+                                    name='unique_proc_recipient_comment')
+        ]
