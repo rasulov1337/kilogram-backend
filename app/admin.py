1c33de97281bafeb5b52a054fe8c7f97417f893e
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-08 23:55:03 +0300

    Delete File model from admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Recipient, FileTransfer, File
+from .models import Recipient, FileTransfer
 
 admin.site.register(Recipient)
 admin.site.register(FileTransfer)
-admin.site.register(File)
