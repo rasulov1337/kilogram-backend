9d982e26f2e90913c447a05a61740b2741193fcf
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-19 23:13:03 +0300

    Update models

--- app/models.py
+++ app/models.py
@@ -3,19 +3,18 @@ from django.db import models
 
 
 class Recipient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     STATUS_CHOICES = [
         ('A', 'Active'),
         ('D', 'Deleted')
     ]
     name = models.CharField(max_length=90)
-    desc = models.CharField(max_length=140, blank=True)
+    desc = models.CharField(max_length=140)
     phone = models.CharField(max_length=18, unique=True)
     city = models.CharField(max_length=40)
-    birthdate = models.DateField(blank=True, null=True)
+    birthdate = models.DateField()
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     avatar = models.URLField(blank=True, null=True)
-    uni = models.CharField(max_length=140, blank=True, null=True)
+    uni = models.CharField(max_length=140)
 
     def __str__(self):
         return self.name
@@ -41,10 +40,10 @@ class FileTransfer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     formed_at = models.DateTimeField(blank=True, null=True)
     completed_at = models.DateTimeField(blank=True, null=True)
-    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='sender')
-    moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='moderator')
+    sender = models.ForeignKey(User, on_delete=models.PROTECT, related_name='sender')
+    moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='moderator')
     recipients = models.ManyToManyField(Recipient, through="FileTransferRecipient")
-    file = models.URLField()
+    file = models.URLField(blank=True, null=True)
 
     objects = FileTransferManager()
 
@@ -61,7 +60,7 @@ class FileTransfer(models.Model):
 class FileTransferRecipient(models.Model):
     file_transfer = models.ForeignKey(FileTransfer, on_delete=models.CASCADE)
     recipient = models.ForeignKey(Recipient, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=200, blank=True)
+    comment = models.CharField(max_length=200, blank=True, null=True)
 
     class Meta:
         constraints = [
